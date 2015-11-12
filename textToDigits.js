329fd81fb77a2f5b3c7a682da3223b8463b391ee
Will Haering <wchaering@gmail.com>
2015-11-11 19:18:21 -0500

    Separated Tests

--- textToDigits.js
+++ textToDigits.js
@@ -34,10 +34,3 @@ String.prototype.convertToDigits = function(){
 		digits *= -1;
 	return digits;
 }
-
-console.log('asdfasdfas'.convertToDigits() == 'No numbers found!' ? 'Pass' : 'Fail');
-console.log('zero one'.convertToDigits() == 1 ? 'Pass' : 'Fail');
-console.log('fourty'.convertToDigits() == 40 ? 'Pass' : 'Fail');
-console.log('negative one hundred and nineteen'.convertToDigits() == -119 ? 'Pass' : 'Fail');
-console.log('negative one hundred billion one'.convertToDigits() == -100000000001 ? 'Pass' : 'Fail');
-console.log('negative three hundred twenty two billion two hundred ninety nine million one hundred twenty nine thousand one hundred twenty three'.convertToDigits() == -322299129123 ? 'Pass' : 'Fail');

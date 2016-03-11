a9b88717e6569142b12983a2fe075d401d93d054
Will Haering <wchaering@Cumulus.fios-router.home>
2016-03-11 09:44:12 -0500

    Fixed Spelling

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 console.log('asdfasdfas'.convertToDigits() === false ? 'Pass' : 'Fail');
 console.log('zero one'.convertToDigits() === 1 ? 'Pass' : 'Fail');
-console.log('fourty'.convertToDigits() === 40 ? 'Pass' : 'Fail');
+console.log('forty'.convertToDigits() === 40 ? 'Pass' : 'Fail');
 console.log('negative one hundred and nineteen'.convertToDigits() === -119 ? 'Pass' : 'Fail');
 console.log('negative one hundred billion one'.convertToDigits() === -100000000001 ? 'Pass' : 'Fail');
 console.log('negative three hundred twenty two billion two hundred ninety nine million one hundred twenty nine thousand one hundred twenty three'.convertToDigits() === -322299129123 ? 'Pass' : 'Fail');
